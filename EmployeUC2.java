506c5b8ec7dab0bedd2b5ef01b579ca6767600c4
siddiq76 <77197840+siddiq76@users.noreply.github.com>
2021-02-02 12:44:28 +0530

    Update EmployeUC2.java

--- EmployeUC2.java
+++ EmployeUC2.java
@@ -1,6 +1,6 @@
 public class EmployeUC2
 {
-	public static void main(String ().args) {
+	public static void main(String[] args) {
 	  //Constants
 	  int IS_FULL_TIME =1;
 	  int EMP_RATE_PER_HOUR = 20;
